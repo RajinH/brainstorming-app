b8dbe5ff3111a1a64f62708ca909d794634eb288
Rajin Hossain <38574408+RajinH@users.noreply.github.com>
2023-12-29 22:05:18 +1100

    Implement reactflow and simple project structure

--- brainstorm-app/src/App.tsx
+++ brainstorm-app/src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Mindmap } from "./features/mindmap/components/mindmap";
 
 function App() {
   return (
     <div>
-      <h1>brainstorm-app</h1>
-      <p>
-        This is just a placeholder for now and will be replaced with content
-      </p>
+      <div className="NavBar">
+        <h1>brainstorm-app</h1>
+        <p>
+          This is just a placeholder for now and will be replaced with content
+        </p>
+      </div>
+      <div className="ExperienceContainer">
+        <Mindmap />
+      </div>
     </div>
   );
 }
