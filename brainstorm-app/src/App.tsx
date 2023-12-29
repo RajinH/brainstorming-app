fa3947c754dbfacd06f81a6e901566ba4a65a92e
Rajin Hossain <38574408+RajinH@users.noreply.github.com>
2023-12-29 21:44:42 +1100

    Reinitialise project with vanilla react and firebase hosting

---
+++ brainstorm-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>brainstorm-app</h1>
+      <p>
+        This is just a placeholder for now and will be replaced with content
+      </p>
+    </div>
+  );
+}
+
+export default App;
